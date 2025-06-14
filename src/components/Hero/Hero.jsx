c8898416831125c56108bee3d21c12b319f66457
Kaushi Kavindaya <amkaushi7@gmail.com>
2025-06-14 19:31:32 +0530

    Updated some files

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image1 from "../../assets/hero/women.png";
+import Image2 from "../../assets/hero/shopping.png";
+import Image3 from "../../assets/hero/sale.png";
+
+const ImageList = [
+    {
+      id :1,
+      img : Image1,
+      title : "Upto 50% off on all Men's Wear",
+      description : "loream His will forever be changed dolar sit amet, consectetur adipiscing elit, sed do eiusmed tempor incidunt ut labore ",
+    },
+  
+    {
+      id :2,
+      img : Image2,
+      title : "Upto 30% off on all Women's Wear",
+      description : "loream His will forever be changed dolar sit amet, consectetur adipiscing elit, sed do eiusmed tempor incidunt ut labore ",
+    },
+  
+    {
+      id :3,
+      img : Image3,
+      title : "Upto 70% off on all Product Sale",
+      description : "loream His will forever be changed dolar sit amet, consectetur adipiscing elit, sed do eiusmed tempor incidunt ut labore ",
+    },
+  ]
+
+const Hero = () => {
+    return <div>Hero</div>;
+};
+
+export default Hero;
