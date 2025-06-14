6fd75da6405f629f5eebbe5a881341ad0beb2eef
Kaushi Kavindaya <amkaushi7@gmail.com>
2025-06-15 02:50:43 +0530

    Updated some files

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,7 +27,29 @@ const ImageList = [
   ]
 
 const Hero = () => {
-    return <div>Hero</div>;
+    return (
+      <div className="relative overflow-hidden min-h-[550px] sm:min-h[650px] bg-gray-100 flex justify-center 
+      items-center dark:bg-gray-950 dark:text-white duration-200">
+        {/*  background patton */}
+        <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9">
+        </div>
+        {/* hero section */}
+        <div className="container pb-8 sm:pb-0">
+          <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2">
+              {/* text content section */}
+              <div>
+                <h1>Loream ipsum dolar sit.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores debitis laborum modi cum delectus. 
+                  Facere atque est molestias dolorem quas, laborum perspiciatis? Cupiditate dolores obcaecati dolorem 
+                  adipisci at repellat voluptate!</p>
+              </div>
+              {/* text content section */}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 };
 
 export default Hero;
